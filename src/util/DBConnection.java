8b4f2b5ec93491713a7924b33d39a7e3e3850862
ASHISH KUMAR BHOI <33381078+AshishBhoi@users.noreply.github.com>
2019-10-12 11:14:33 +0530

    Jsp Project Template with: - JSP - SRC - WebContent - Bootstrap

---
+++ src/util/DBConnection.java
@@ -0,0 +1,26 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection createConnection() {
+        Connection con = null;
+        String url = "jdbc:postgresql://localhost:5432/group1"; //PostgreSql URL and followed by the database name
+        String username = "ashish"; //PostgreSql username
+        String password = "12345678"; //PostgreSql password
+
+        try {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            con = DriverManager.getConnection(url, username, password); //attempting to connect to MySQL database
+            System.out.println("Printing connection object " + con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
